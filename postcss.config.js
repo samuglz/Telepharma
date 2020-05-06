743c7ba1a86d812354bf7476110a17bb8d6362e2
Samuel Glez <samuelgonzalezalonso@gmail.com>
2020-05-06 20:20:15 +0100

    First Commit

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+const tailwindcss = require('tailwindcss');
+const postcssPurgecss = require(`@fullhuman/postcss-purgecss`);
+const postcssClean = require('postcss-clean');
+
+const purgecss = postcssPurgecss({
+   content: ['./public/**/*.html', './src/**/*.vue'],
+
+   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+
+   whitelistPatterns: [
+      /-(leave|enter|appear)(|-(to|from|active))$/,
+      /^(?!(|.*?:)cursor-move).+-move$/,
+      /^router-link(|-exact)-active$/
+   ]
+});
+
+module.exports = {
+   plugins: [
+      tailwindcss,
+      postcssClean,
+      ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+   ]
+};
