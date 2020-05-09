b10e3a0bb4bc9f9b738d44488454fb39bc92f1ad
samuglz <samuelgonzalezalonso@gmail.com>
2020-05-09 15:54:44 +0100

    Eliminado paquete dotenv

--- src/services/databaseService.js
+++ src/services/databaseService.js
@@ -1,59 +1,50 @@
-require('dotenv').config();
 const axios = require('axios');
+const BASE_API = 'http://localhost:3001';
 
 class dbService {
    async getAffiliate(options = {}) {
-      const response = await axios.get(`${process.env.BASEAPI}/affiliates`, {
+      const response = await axios.get(`${BASE_API}/affiliates`, {
          params: options
       });
       return response.data;
    }
 
    async getUser(options = {}) {
-      const response = await axios.get(`${process.env.BASEAPI}/users`, {
+      const response = await axios.get(`${BASE_API}/users`, {
          params: options
       });
       return response.data;
    }
 
    async getProduct(options = {}) {
-      const response = await axios.get(`${process.env.BASEAPI}/products`, {
+      const response = await axios.get(`${BASE_API}/products`, {
          params: options
       });
       return response.data;
    }
 
    async newUser(userData) {
-      const response = await axios.post(
-         `${process.env.BASEAPI}/register`,
-         userData
-      );
+      const response = await axios.post(`${BASE_API}/register`, userData);
       return response.data;
    }
 
    async newProduct(productData) {
-      const response = await axios.post(
-         `${process.env.BASEAPI}/products`,
-         productData
-      );
+      const response = await axios.post(`${BASE_API}/products`, productData);
       return response.data;
    }
 
    async newAffiliate(affiliateData) {
       const response = await axios.post(
-         `${process.env.BASEAPI}/affiliates`,
+         `${BASE_API}/affiliates`,
          affiliateData
       );
       return response.data;
    }
 
    async login(userData) {
-      const response = await axios.post(
-         `${process.env.BASEAPI}/login`,
-         userData
-      );
+      const response = await axios.post(`${BASE_API}/login`, userData);
       return response.data;
    }
 }
 
-export default dbService;
+module.exports = dbService;
