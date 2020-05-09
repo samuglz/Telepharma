823be8b02e0eca0e2cd95fc215df5128961d9c6d
samuglz <samuelgonzalezalonso@gmail.com>
2020-05-09 15:15:27 +0100

    Cambiadas nombre de varias funciones del servicio

---
+++ src/services/databaseService.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const axios = require('axios');
+
+class dbService {
+   async getAffiliate(options = {}) {
+      const response = await axios.get(`${process.env.BASEAPI}/affiliates`, {
+         params: options
+      });
+      return response.data;
+   }
+
+   async getUser(options = {}) {
+      const response = await axios.get(`${process.env.BASEAPI}/users`, {
+         params: options
+      });
+      return response.data;
+   }
+
+   async getProduct(options = {}) {
+      const response = await axios.get(`${process.env.BASEAPI}/products`, {
+         params: options
+      });
+      return response.data;
+   }
+
+   async newUser(userData) {
+      const response = await axios.post(
+         `${process.env.BASEAPI}/register`,
+         userData
+      );
+      return response.data;
+   }
+
+   async newProduct(productData) {
+      const response = await axios.post(
+         `${process.env.BASEAPI}/products`,
+         productData
+      );
+      return response.data;
+   }
+
+   async newAffiliate(affiliateData) {
+      const response = await axios.post(
+         `${process.env.BASEAPI}/products`,
+         affiliateData
+      );
+      return response.data;
+   }
+}
+
+export default dbService;
