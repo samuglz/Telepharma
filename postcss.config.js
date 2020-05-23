6a7c2928d2720e0d59b5fba7ba284e1f9f3e68da
samuglz <samuelgonzalezalonso@gmail.com>
2020-05-23 22:31:55 +0100

    Desactivando PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,23 @@
 const tailwindcss = require('tailwindcss');
-const postcssPurgecss = require(`@fullhuman/postcss-purgecss`);
+// const postcssPurgecss = require(`@fullhuman/postcss-purgecss`);
 const postcssClean = require('postcss-clean');
 
-const purgecss = postcssPurgecss({
-   content: ['./public/**/*.html', './src/**/*.vue'],
+// const purgecss = postcssPurgecss({
+//    content: ['./public/**/*.html', './src/**/*.vue'],
 
-   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+//    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
 
-   whitelistPatterns: [
-      /-(leave|enter|appear)(|-(to|from|active))$/,
-      /^(?!(|.*?:)cursor-move).+-move$/,
-      /^router-link(|-exact)-active$/
-   ]
-});
+//    whitelistPatterns: [
+//       /-(leave|enter|appear)(|-(to|from|active))$/,
+//       /^(?!(|.*?:)cursor-move).+-move$/,
+//       /^router-link(|-exact)-active$/
+//    ]
+// });
 
 module.exports = {
    plugins: [
       tailwindcss,
-      postcssClean,
-      ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+      postcssClean
+      // ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
    ]
 };
